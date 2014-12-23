fdb5df60631762e82b21e81e47a355798bfe818f
davidfurnier8111 <davidf1991@msn.com>
2014-12-24 00:23:08 +0100

    Bower_components to .. directory (for the demo page)

--- process.php
+++ process.php
@@ -9,8 +9,8 @@
 	$message = 'Mensaje de ' . $name . 'con email de contacto ' . $email . ' .\r\n\n'
                'Cuerpo del mensaje:\r\n' . $message;
 	$headers = 'From: ' 		. mysql_escape_string('prueba@no-reply.com') . '\r\n'
-			 . 'Reply-To: '		. mysql_escape_string('prueba@no-reply.com') . '\r\n'
-			 . 'X-Mailer: PHP/'	. phpversion();
+		. 'Reply-To: '		. mysql_escape_string('prueba@no-reply.com') . '\r\n'
+		. 'X-Mailer: PHP/'	. phpversion();
 
 	if (!mail($to_email,$subject,$message,$headers))
 	{
